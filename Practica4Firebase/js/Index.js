625738bbf9aa42340d57aef807ef938234d0f9f7
JeanVaca <61167351+JeanVaca@users.noreply.github.com>
2020-05-20 13:54:15 -0500

    Update Index.js

--- Practica4Firebase/js/Index.js
+++ Practica4Firebase/js/Index.js
@@ -1,51 +1,50 @@
-const datosdelacuenta = document.querySelector('.datosdelacuenta');
 const listaloggedout = document.querySelectorAll('.logged-out');
 const listaloggedin = document.querySelectorAll('.logged-in');
-
-const configurarMenu = (user)=>{
+const datosdelacuenta = document.querySelector('.datosdelacuenta');
+ 
+const configuraMenu = (user) => {
     if(user){
-        db.collection('usuarios').doc(user.uid).get().then(doc =>{
+        db.collection('usuarios').doc(user.uid).get().then( doc =>{
             const html = `
-            <p>Nombre: ${ doc.data().nombre } </p>
-            <p>Correo: ${ user.email } </p>
-            <p>Telefono: ${ doc.data().telefono } </p>
-            <p>Direccion: ${ doc.data().direccion } </p>
-            <p>Coordenadas: ${ doc.data().coordenadas.latitude }, ${ doc.data().coordenadas.longitude } </p>
+                <p>Nombre: ${ doc.data().nombre }</p>
+                <p>Correo: ${ user.email}</p>
+                <p>Teléfono: ${ doc.data().telefono }</p>
+                <p>Dirección: ${ doc.data().direccion }</p>
+                <p>Coordenadas: ${ doc.data().coordenadas.latitude}, ${ doc.data().coordenadas.longitude }</p>
             `;
-
             datosdelacuenta.innerHTML = html;
         });
-
-        listaloggedin.forEach(item => item.style.display = 'block');
-        listaloggedout.forEach(item => item.style.display = 'none');
-    }else{
-        listaloggedin.forEach(item => item.style.display = 'none');
-        listaloggedout.forEach(item => item.style.display = 'block');
+        listaloggedin.forEach( item => item.style.display = 'block');
+        listaloggedout.forEach( item => item.style.display = 'none');
     }
-};
-
-const listaproductos = document.getElementById('listaproductos');
-
-const obtenerProductos = (data) => {
-    if(data.length){
-        let html = '';
-        data.forEach(doc =>{
-            const producto = doc.data();
-            const columna = `
-            <div class="col-12 col-md-3"> 
-                <img src="img/${producto.imagen}" alt="${producto.nombre}"> 
-                <p>${producto.nombre}</p> 
-                <p class="text-danger">$${producto.precio}</p> 
-                <a href="https://www.paypal.me/grupohernandezalba/${producto.precio}" target="_blank">
-                    <button class="btn btn-primary">Pagar Ahora</button>
-                </a>
-            </div>
-            `;
-
-            html += columna;
-        });
-        listaproductos.innerHTML = html;
-    }else{
-        listaproductos.innerHTML = `<p class="text-center">Ingresa con tu cuenta para poder ver nuestros productos</p>`
+    else
+    {
+        datosdelacuenta.innerHTML = '';
+        listaloggedin.forEach( item => item.style.display = 'none');
+        listaloggedout.forEach( item => item.style.display = 'block');
     }
+ }
+
+const obtieneAmigos = (data) => {
+    var propiedades = {
+        center: {
+            lat: 19.420847, lng: -102.062763
+        },
+        zoom: 14
+    }
+    var mapa = document.getElementById("map")
+    var map = new google.maps.Map(mapa, propiedades);
+
+    data.forEach( doc => {
+        informacion = new google.maps.InfoWindow;
+
+        var pos = {
+            lat: doc.data().coordenadas.latitude,
+            lng: doc.data().coordenadas.longitude
+        };
+
+        informacion.setPosition(pos);
+        informacion.setContent(doc.data().nombre);
+        informacion.open(map);
+    });
 };
