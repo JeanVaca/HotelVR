ea6ff021f2369c50b4b3804ef4023d6826c6b0b0
JeanVaca <61167351+JeanVaca@users.noreply.github.com>
2022-06-01 22:11:10 -0500

    Delete Firebase directory

--- Firebase/agregar.js
+++
@@ -1,8 +0,0 @@
-formulario.addEventListener('submit',(e)=> {
-        e.preventDefault();
-        var registro = new Registro(null,formulario.nombre.value,formulario.codigo.value);
-        registro.agregar();
-        formulario.nombre.value ='';
-        formulario.codigo.value ='';
-        $('#ventananuevo').modal('hide');
-    });
