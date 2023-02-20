0a74294fe48f7df22f01a6b1af5286c218f17301
JeanVaca <61167351+JeanVaca@users.noreply.github.com>
2023-02-20 04:00:48 -0600

    Update formularios.js

--- js/formularios.js
+++ js/formularios.js
@@ -1,3 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('works');
 })
+
+const taskForm = document.getElementById('task-form')
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    console.log('submited')
+})
